23477e34b868b7d8f20124b6c817e3bda4f937d2
Letty <lettinwonderland@gmail.com>
2022-09-21 23:23:40 -0300

    positivo e negativo

---
+++ exsfeitoemsala/testeCondicionais/alg12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{  
+    
+    int n = 0;
+    scanf("%d", &n);
+    
+    if (n>0){
+        printf("É positivo");
+        
+    }
+    else {
+        printf("É negativo");
+    }
+    
+    return 0;
+}
