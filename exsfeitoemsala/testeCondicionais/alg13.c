4040f8845c954ee7ae09c8c03ebe43fcd3f1c7cc
Letty <lettinwonderland@gmail.com>
2022-09-21 23:24:20 -0300

    par ou impar

---
+++ exsfeitoemsala/testeCondicionais/alg13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+
+    int n = 0;
+    scanf("%d", &n);
+
+    if (n % 2 == 0)
+    {
+        printf("É par.");
+    }
+    else
+    {
+        printf("É ímpar.");
+    }
+
+    return 0;
+}
