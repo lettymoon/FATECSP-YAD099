247bcd88fb63d0e7babb539ebd7e5522b4c16643
Letty <lettinwonderland@gmail.com>
2022-09-28 18:28:09 -0300

    soma de dois numeros inteiros

---
+++ exsfeitoemsala/operacoesNumericas/alg1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    //criar 3 variaveis para guardar numeros inteiros
+    int a =0, b = 0, c = 0;
+    scanf("%d %d", &a, &b);
+
+    //somar os 2 números e guardá-los na terceira variável
+    c = a + b;
+
+    //imprimir na tela o valor da terceira variável
+    printf("%d", c);
+
+    return 0;
+}
