ded6d4aee4dd4386d257ea21a3079c01b52d5f3b
Letty <lettinwonderland@gmail.com>
2022-12-06 12:27:50 -0300

    num primo

--- exsfeitoemsala/simulado/main4.c
+++ exsfeitoemsala/simulado/main4.c
@@ -1,3 +1,17 @@
+/*4) Faça um algoritmo em C que descubra se um número é ou não é PRIMO. Um número primo é definido como tendo exatamente 2 divisores; assim, 
+use o exercício anterior para lhe ajudar!!
+
+Teste:
+-100: Não é primo
+-49: Não é primo
+-121: Não é primo
+-45: Não é primo
+-400: Não é primo
+-7: É primo
+-11: É primo
+-13: É primo
+-1: Não é primo (só tem 1 divisor, e não 2!!!)*/
+
 #include <stdio.h>
 
 int main(){
