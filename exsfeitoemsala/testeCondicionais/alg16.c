5cf1d2cb415d7656e36251e78a99a289929a7266
Letty <lettinwonderland@gmail.com>
2022-12-06 12:28:18 -0300

    baskhara

--- exsfeitoemsala/testeCondicionais/alg16.c
+++ exsfeitoemsala/testeCondicionais/alg16.c
@@ -3,29 +3,24 @@
 
 int main() {
 
-    float a = 0;
-    float b = 0;
-    float c = 0;
+float a, b, c;
+scanf("%f %f %f", &a, &b, &c);
 
-    scanf("%f %f %f", &a, &b, &c);
+float delta = pow(b,2) - (4*a*c);
+if (delta < 0)
+  printf("Não há raízes reais.");
+else{
+if(delta > 0){
+  float raizdelta = sqrt(delta);
+  float x1 = (-b + raizdelta) / (2*a);
+  float x2 = (-b - raizdelta) / (2*a);
+  printf("x1 = %.2f\n", x1);
+  printf("x2 = %.2f\n", x2);
+}else{
+  float x = (-b) / (2*a);
+  printf("x = %.2f\n", x);
+  }  
+}
 
-    float delta = pow(b,2) - 4*a*c;
-
-    if (delta < 0){
-        printf("Não há raízes reais.");
-    }else{
-        if(delta > 0){
-            float raizdelta = sqrt(delta);
-            float x1 = (-b + raizdelta) / 2*a;
-            float x2 = (-b - raizdelta) / 2*a;
-
-            printf("%f", x1);
-            printf("%f", x2);
-        }else{
-            float x = (-b) / (2*a);
-            printf("%f", x);
-        }           
-    }
-
-    return 0;
+return 0;
 }
