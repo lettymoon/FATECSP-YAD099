ef117c2fc5ea879c4e3e1145d3d409651b9ba249
Letty <lettinwonderland@gmail.com>
2022-12-06 12:27:29 -0300

    divisores de um num n

--- exsfeitoemsala/simulado/main3.c
+++ exsfeitoemsala/simulado/main3.c
@@ -1,3 +1,19 @@
+/*
+3) Faça um algoritmo em C que leia um número inteiro e mostre todos os seus divisores. 
+
+Teste:
+-10: 1, 2, 5, 10
+-100: 1, 2, 4, 5, 10, 20, 50, 100
+-49: 1, 7, 49
+-121: 1, 11, 121
+-45: 1, 3, 5, 15, 45
+-400: 1, 2, 4, 5, 8, 10, 20, 40, 50, 100, 200, 400
+-7: 1, 7
+-11: 1, 11
+-13: 1, 13
+-1: 1
+*/
+
 #include <stdio.h>
 
 int main (){
