8ee8b1df02f9f8158f7cfe89d46e07bdec5eabbb
Letty <lettinwonderland@gmail.com>
2022-12-06 12:26:31 -0300

    fatorial

--- exsfeitoemsala/simulado/main.c
+++ exsfeitoemsala/simulado/main.c
@@ -1,3 +1,13 @@
+/*1) Faça um algoritmo em C que calcule o fatorial de um digitado:
+
+	0! = 1
+	1! = 1
+	2! = 1*2 = 2
+	3! = 1*2*3 = 6
+	4! = 1*2*3*4 = 24
+	5! = 1*2*3*4*5 = 120
+	n! = 1*2*3*4*5*6*7*8*9* .....*(n-2)*(n-1)*n*/
+
 #include <stdio.h>
 
 int main (){
